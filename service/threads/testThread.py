21eeb21810e9bf4be1477cae93506a4bce8e6762
Ebag333 <ebagola@gmail.com>
2017-04-11 08:36:15 -0700

    Testing threading

---
+++ service/threads/testThread.py
@@ -0,0 +1,9 @@
+import os
+from importlib import import_module
+
+
+def threadedEosEffectsImport():
+    # Walk eos.effects and add all effects so we can import them properly
+    print("Starting Test Thread")
+
+    print("Completed Test Thread")
