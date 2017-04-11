4c587a0fd5e28510aac2d1d8d2bb99149d2b8f0d
Ebag333 <ebagola@gmail.com>
2017-04-11 15:26:54 -0700

    re-enable threading

--- pyfa.py
+++ pyfa.py
@@ -28,7 +28,7 @@ from imp import find_module
 from optparse import AmbiguousOptionError, BadOptionError, OptionParser
 from multiprocessing import freeze_support
 
-# from service.serviceThreads import executeStartupThreads
+from service.serviceThreads import executeStartupThreads
 
 from logbook import CRITICAL, DEBUG, ERROR, FingersCrossedHandler, INFO, Logger, NestedSetup, NullHandler, StreamHandler, TimedRotatingFileHandler, WARNING, \
     __version__ as logbook_version
@@ -381,7 +381,7 @@ if __name__ == "__main__":
         eos.db.saveddata_meta.create_all()
 
         pyfalog.info("Starting threads")
-        # executeStartupThreads()
+        executeStartupThreads()
 
         from gui.mainFrame import MainFrame
 
