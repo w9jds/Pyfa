41b55d9e297ab750f148da23fc01ae415ee38ffc
Ebag333 <ebagola@gmail.com>
2017-05-01 17:01:59 -0700

    Set capacitor booster reload time

--- eos/saveddata/module.py
+++ eos/saveddata/module.py
@@ -361,12 +361,12 @@ class Module(HandledItem, HandledCharge, ItemAttrShortcut, ChargeAttrShortcut):
             moduleReloadTime = self.getModifiedItemAttr("reloadTime")
             if moduleReloadTime is None:
                 moduleReloadTime = self.__reloadTime
+
+            if moduleReloadTime is None and getattr(self.item.group, 'name', None) == 'Capacitor Booster':
+                # Cap boosters don't have reload times, so manually set to 10 seconds
+                moduleReloadTime = 10
         else:
             moduleReloadTime = 0
-
-        if moduleReloadTime is None and getattr(self.item.group, 'name', None) == 'Capacitor Booster':
-            # Cap boosters don't have reload times, so manually set one
-            moduleReloadTime = 10
 
         return moduleReloadTime
 
