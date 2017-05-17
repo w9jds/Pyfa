89cc79a3b11359b928765f2c006a17625c34f615
Ebag333 <ebagola@gmail.com>
2017-05-17 00:35:50 -0700

    Catch where settings path could have issues

--- service/settings.py
+++ service/settings.py
@@ -45,8 +45,12 @@ class SettingsProvider(object):
             if hasattr(self, 'BASE_PATH'):
                 if not os.path.exists(self.BASE_PATH):
                     os.mkdir(self.BASE_PATH)
+            else:
+                # Store settings at root
+                self.BASE_PATH = ""
         except OSError:
             pyfalog.warning("Could not create settings path.")
+            # Store settings at root
             self.BASE_PATH = ""
 
     def getSettings(self, area, defaults=None):
