561373ff4b345ea8168fdee800a77455e75326d1
Ebag333 <ebagola@gmail.com>
2017-05-27 03:53:25 -0700

    Fix where refactor of name ended up with duplicate name

--- eos/effects/moduletitaneffectgenerator.py
+++ eos/effects/moduletitaneffectgenerator.py
@@ -9,7 +9,7 @@ def handler(fit, container, context, **kwargs):
     for x in xrange(1, 5):
         if container.getModifiedItemAttr("warfareBuff{}ID".format(x)):
             value = container.getModifiedItemAttr("warfareBuff{}Value".format(x))
-            container = container.getModifiedItemAttr("warfareBuff{}ID".format(x))
+            id = container.getModifiedItemAttr("warfareBuff{}ID".format(x))
 
-            if container:
-                fit.addCommandBonus(container, value, container, kwargs['effect'])
+            if id:
+                fit.addCommandBonus(id, value, container, kwargs['effect'])
